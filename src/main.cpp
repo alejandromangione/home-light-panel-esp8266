320b1d6be76556f6552267ca5d36c9458c9b76d5
Alejandro Mangione <alejandro@vericatch.com>
2020-10-18 02:16:18 -0700

    added Adafruit_NeoPixel lib and tested leds

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,43 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
 
-// #define PIN 6
+#define LED_COUNT 150
+#define PIN D2
+
+Adafruit_NeoPixel strip(LED_COUNT, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(LED_BUILTIN, OUTPUT);
+
+  strip.begin();
+  strip.setBrightness(5);
+  strip.show();
+
+  Serial.println("=========== Init");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(2000);
+  Serial.println("=========== Start Loop");
 
   digitalWrite(LED_BUILTIN, LOW);
   delay(500);
 
-  Serial.println("Loop");
-  // Serial.println("Loop");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+
+  for (int i = 0; i < strip.numPixels(); i++) {
+    Serial.println(i);
+
+    strip.setPixelColor(i, strip.Color(255, 0, 0 ));
+    strip.show();
+    delay(100);
+
+    strip.clear();
+    strip.show();
+    delay(50);
+  }
+
+  Serial.println("=========== End Loop");
 }
